da0b51b58dbc629abaa426c2ecff46de1bd3c161
SMART-TN-038 <blue672@naver.com>
2023-04-13 17:43:36 +0900

    kafka producer, consumer 추가

---
+++ kafka-server/src/main/java/com/kafka/server/controller/KafkaController.java
@@ -0,0 +1,27 @@
+package com.kafka.server.controller;
+
+
+import com.kafka.server.service.KafkaProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/kafka")
+public class KafkaController {
+    private final KafkaProducer producer;
+
+    @Autowired
+    KafkaController(KafkaProducer producer) {
+        this.producer = producer;
+    }
+
+    @PostMapping
+    public String sendMessage(@RequestParam("message") String message) {
+        this.producer.sendMessage(message);
+
+        return "success";
+    }
+}
